bff97cf16905166fbef46c4a8f8b8a343be92f86
gitMike000 <76992900+gitMike000@users.noreply.github.com>
2022-04-02 11:55:38 +0300

    Delete Задание 4 directory

--- Задание 4/start4.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-# Запустить в отдельной консоли
-#netcat -u -l -p 10000
-echo "ftp://admin:1234@91.122.30.115 21"
-
-LD_PRELOAD=./libcall-intercepter.so ftpcopy -u admin -p 1234 ftp://91.122.30.115:21 .
