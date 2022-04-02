001c34268387c3e6e5c25861850f4eec815f6588
gitMike000 <76992900+gitMike000@users.noreply.github.com>
2022-04-02 11:55:29 +0300

    Delete Задание 1 3 directory

--- Задание 1 3/start3.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-xfce4-terminal -e "/bin/bash -c \"netcat -u -l -p 11111\""
-xfce4-terminal -e "/bin/bash -c \"netcat -u -l -p 10000\""
-LD_PRELOAD=./libcall-intercepter.so netcat -u localhost 11111
